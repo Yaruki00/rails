783ed61d5365c2a005f48f0665a349172e694517
Yuta Kawabe <yuyu3165@gmail>
2013-01-26 01:06:47 +0900

    create db seed

--- books/db/seeds.rb
+++ books/db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#coding: utf-8
+
+Title.create(:name => 'なまえ', :sales_date => '2000-01-01')
+Title.create(:name => 'ほん', :sales_date => '9999-12-31')
